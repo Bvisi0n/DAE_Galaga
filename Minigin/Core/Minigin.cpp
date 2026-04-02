4297af3e992082ea42d978e73ab8357a194ee428
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-02 18:00:28 +0200

    Another Emscripten patch!
    Hopefully the last one in this refactor.

--- Minigin/Core/Minigin.cpp
+++ Minigin/Core/Minigin.cpp
@@ -42,7 +42,7 @@ void LogSDLVersion(const std::string& message, int major, int minor, int patch)
 
 	void LoopCallback(void* arg)
 	{
-		static_cast<dae::Minigin*>(arg)->RunOneFrame();
+		static_cast<dae::core::Minigin*>(arg)->RunOneFrame();
 	}
 #endif
 
