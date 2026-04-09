34e719f77a7394b6a9f16512327537ee1e67cce8
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-09 12:11:57 +0200

    Add primitive renderer and viewport border

--- Game/Builders/GravityBenderBuilder.h
+++ Game/Builders/GravityBenderBuilder.h
@@ -3,9 +3,12 @@
 
 #include <memory>
 
+#include "SDL3/SDL.h"
+
 #include "Game/Components/FPSComponent.h"
 
 #include "Minigin/Core/GameObject.h"
+#include "Minigin/Graphics/PrimitiveRenderComponent.h"
 #include "Minigin/Graphics/TextComponent.h"
 #include "Minigin/Graphics/TextureComponent.h"
 #include "Minigin/Resources/ResourceManager.h"
@@ -21,6 +24,7 @@ namespace bvi::builders
 		static void Build( dae::scenes::Scene& scene )
 		{
 			BuildBackground( scene );
+			BuildViewportBorder( scene );
 			BuildFPSCounter( scene );
 			scene.Initialize();
 		}
@@ -31,6 +35,18 @@ namespace bvi::builders
 			auto background{ std::make_unique<dae::core::GameObject>() };
 			background->AddComponent<dae::graphics::TextureComponent>()->SetTexture( "background.png" );
 			scene.AddGameObject( std::move( background ) );
+		}
+
+		static void BuildViewportBorder( dae::scenes::Scene& scene )
+		{
+			// TODO H: Fetch screen dimensions and use them.
+			constexpr SDL_FRect screenBounds{ 0.f, 0.f, 1024.f, 576.f };
+			constexpr SDL_Color neonPurple{ 128, 0, 128, 255 };
+
+			auto borderObject{ std::make_unique<dae::core::GameObject>() };
+			borderObject->AddComponent<dae::graphics::PrimitiveRenderComponent>( screenBounds, neonPurple );
+
+			scene.AddGameObject( std::move( borderObject ) );
 		}
 
 		static void BuildFPSCounter( dae::scenes::Scene& scene )
---
+++ Minigin/Graphics/PrimitiveRenderComponent.h
@@ -0,0 +1,55 @@
+#ifndef PRIMITIVERENDERCOMPONENT_H
+#define PRIMITIVERENDERCOMPONENT_H
+
+#include <vector>
+
+#include <SDL3/SDL_render.h>
+#include "SDL3/SDL.h"
+
+#include "Minigin/Core/Component.h"
+#include "Minigin/Graphics/IRenderable.h"
+#include "Minigin/Graphics/Renderer.h"
+
+// Expand as needed, I kept the name generic, it should use some form of state to setup it's behaivour, don't want seperate components for each primitive type. (mentioned in State Pattern in the book)
+
+namespace dae::graphics
+{
+	class PrimitiveRenderComponent final : public core::Component, public IRenderable
+	{
+	public:
+		PrimitiveRenderComponent( core::GameObject* owner, const SDL_FRect& bounds, const SDL_Color& color )
+			: Component( owner )
+			, m_bounds{ bounds }
+			, m_color{ color }
+		{}
+
+		virtual ~PrimitiveRenderComponent() = default;
+
+		PrimitiveRenderComponent( const PrimitiveRenderComponent& ) = delete;
+		PrimitiveRenderComponent( PrimitiveRenderComponent&& ) = delete;
+		PrimitiveRenderComponent& operator=( const PrimitiveRenderComponent& ) = delete;
+		PrimitiveRenderComponent& operator=( PrimitiveRenderComponent&& ) = delete;
+
+		void InitializeLinkage() override
+		{}
+
+		void InitializeState() override
+		{}
+
+		void Update( const float ) override
+		{}
+
+		void Render() const override
+		{
+			SDL_Renderer* renderer{ Renderer::GetInstance().GetSDLRenderer() };
+			if ( !renderer ) return;
+			SDL_SetRenderDrawColor( renderer, m_color.r, m_color.g, m_color.b, m_color.a );
+			SDL_RenderRect( renderer, &m_bounds );
+		}
+
+	private:
+		SDL_FRect m_bounds;
+		SDL_Color m_color;
+	};
+}
+#endif
