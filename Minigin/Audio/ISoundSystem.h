7327faf9a9683f88cb659ac86dc3e4173300b138
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-29 09:43:57 +0200

    Apply rule of 5 to ISoundSystem

--- Minigin/Audio/ISoundSystem.h
+++ Minigin/Audio/ISoundSystem.h
@@ -10,13 +10,28 @@ namespace dae::audio
 	class ISoundSystem
 	{
 	public:
+		ISoundSystem() = default;
 		virtual ~ISoundSystem() = default;
+
+		ISoundSystem( const ISoundSystem& ) = delete;
+		ISoundSystem( ISoundSystem&& ) = default;
+		ISoundSystem& operator=( const ISoundSystem& ) = delete;
+		ISoundSystem& operator=( ISoundSystem&& ) = default;
+
 		virtual void Play( sound_id id, float volume ) = 0;
 	};
 
 	class NullSoundSystem final : public ISoundSystem
 	{
 	public:
+		NullSoundSystem() = default;
+		~NullSoundSystem() override = default;
+
+		NullSoundSystem( const NullSoundSystem& ) = delete;
+		NullSoundSystem( NullSoundSystem&& ) = default;
+		NullSoundSystem& operator=( const NullSoundSystem& ) = delete;
+		NullSoundSystem& operator=( NullSoundSystem&& ) = default;
+
 		void Play( sound_id /*id*/, float /*volume*/ ) override
 		{}
 	};
