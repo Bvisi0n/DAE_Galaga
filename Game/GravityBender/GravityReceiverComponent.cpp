4e3ae7e09a62342d29033af3f0f7e7ba36ad2d19
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-29 11:58:45 +0200

    Split SpawnGravityFieldCommand into .h/.cpp
    I've also discover core guideline SF.12:
    Prefer the quoted form of #include for files relative to the including file and the angle bracket form everywhere else.
    
    Since I use full paths everywhere, they should all become <>.

---
+++ Game/GravityBender/GravityReceiverComponent.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+
+#include <glm/ext/vector_float3.hpp>
+#include <glm/geometric.hpp>
+
+#include <Game/GravityBender/GravityReceiverComponent.h>
+#include <Game/GravityBender/GravityRegistry.h>
+#include <Minigin/Core/Component.h>
+#include <Minigin/Core/GameObject.h>
+#include <Minigin/Core/MoveComponent.h>
+
+namespace bvi::gravity_bender
+{
+	GravityReceiverComponent::GravityReceiverComponent( dae::core::GameObject* owner )
+		: Component( owner )
+	{}
+
+	GravityReceiverComponent::~GravityReceiverComponent() = default;
+
+	void GravityReceiverComponent::InitializeLinkage()
+	{
+		m_moveComponent = GetOwner()->GetComponent<dae::core::MoveComponent>();
+	}
+
+	void GravityReceiverComponent::InitializeState()
+	{}
+
+	void GravityReceiverComponent::Update( const float )
+	{
+		if ( !m_moveComponent )
+		{
+			return;
+		}
+
+		const glm::vec3 currentPos = GetOwner()->GetTransform().GetWorldPosition();
+		glm::vec3 totalGravityForce{ 0.0f, 0.0f, 0.0f };
+
+		constexpr float epsilonSq = 2500.0f;
+
+		for ( const auto& node : GravityRegistry::GetActiveNodes() )
+		{
+			const glm::vec3 diff = node.position - currentPos;
+			const float distanceSq = glm::dot( diff, diff );
+
+			if ( distanceSq < node.radiusSquared )
+			{
+				const float smoothedDistSq = distanceSq + epsilonSq;
+
+				const float invSmoothedDist = 1.0f / std::sqrt( smoothedDistSq );
+				const float invDistCube = ( 1.0f / smoothedDistSq ) * invSmoothedDist;
+
+				totalGravityForce += diff * ( node.strength * invDistCube );
+			}
+		}
+
+		m_moveComponent->AddForce( totalGravityForce );
+	}
+}
--- Game/GravityBender/GravityReceiverComponent.h
+++ Game/GravityBender/GravityReceiverComponent.h
@@ -1,71 +1,31 @@
 #ifndef BVI_GRAVITYRECEIVERCOMPONENT_H
 #define BVI_GRAVITYRECEIVERCOMPONENT_H
 
-#include <cmath>
+#include <Minigin/Core/Component.h>
 
-#include <glm/ext/vector_float3.hpp>
-#include <glm/geometric.hpp>
-
-#include "Game/GravityBender/GravityRegistry.h"
-
-#include <Minigin/Core/GameObject.h>
-#include "Minigin/Core/Component.h"
-#include "Minigin/Core/MoveComponent.h"
+namespace dae::core
+{
+	class GameObject;
+	class MoveComponent;
+}
 
 namespace bvi::gravity_bender
 {
 	class GravityReceiverComponent final : public dae::core::Component
 	{
 	public:
-		explicit GravityReceiverComponent( dae::core::GameObject* owner )
-			: Component( owner )
-		{}
-
-		~GravityReceiverComponent() override = default;
+		explicit GravityReceiverComponent( dae::core::GameObject* owner );
+		~GravityReceiverComponent() override;
 
 		GravityReceiverComponent( const GravityReceiverComponent& ) = delete;
 		GravityReceiverComponent( GravityReceiverComponent&& ) = delete;
 		GravityReceiverComponent& operator=( const GravityReceiverComponent& ) = delete;
 		GravityReceiverComponent& operator=( GravityReceiverComponent&& ) = delete;
 
-		void InitializeLinkage() override
-		{
-			m_moveComponent = GetOwner()->GetComponent<dae::core::MoveComponent>();
-		}
+		void InitializeLinkage() override;
+		void InitializeState() override;
 
-		void InitializeState() override
-		{}
-
-		void Update( const float ) override
-		{
-			if ( !m_moveComponent )
-			{
-				return;
-			}
-
-			const glm::vec3 currentPos = GetOwner()->GetTransform().GetWorldPosition();
-			glm::vec3 totalGravityForce{ 0.0f, 0.0f, 0.0f };
-
-			constexpr float epsilonSq = 2500.0f;
-
-			for ( const auto& node : GravityRegistry::GetActiveNodes() )
-			{
-				const glm::vec3 diff = node.position - currentPos;
-				const float distanceSq = glm::dot( diff, diff );
-
-				if ( distanceSq < node.radiusSquared )
-				{
-					const float smoothedDistSq = distanceSq + epsilonSq;
-
-					const float invSmoothedDist = 1.0f / std::sqrt( smoothedDistSq );
-					const float invDistCube = ( 1.0f / smoothedDistSq ) * invSmoothedDist;
-
-					totalGravityForce += diff * ( node.strength * invDistCube );
-				}
-			}
-
-			m_moveComponent->AddForce( totalGravityForce );
-		}
+		void Update( const float ) override;
 
 	private:
 		dae::core::MoveComponent* m_moveComponent{ nullptr };
