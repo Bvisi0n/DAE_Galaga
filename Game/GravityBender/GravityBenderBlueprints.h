f8ca44322060095aedeb8f81a9aa51970daa4210
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-29 12:34:19 +0200

    Remove unused filename and <string> include from GravBendBlueprints

--- Game/GravityBender/GravityBenderBlueprints.h
+++ Game/GravityBender/GravityBenderBlueprints.h
@@ -1,8 +1,6 @@
 #ifndef BVI_GRAVITYBENDERBLUEPRINTS_H
 #define BVI_GRAVITYBENDERBLUEPRINTS_H
 
-#include <string>
-
 // Inspired by Type Object.
 
 // TODO bvi_gravity_bender - Move all tweakable properties to this file.
@@ -11,7 +9,6 @@ namespace bvi::gravity_bender
 {
 	struct UnitData
 	{
-		std::string filename{ "zako.png" };
 		float speed{ 200.0f };
 		float spawnDelay{ 0.15f };
 		int spawnCount{ 10 };
