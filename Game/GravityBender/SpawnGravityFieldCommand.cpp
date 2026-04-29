7de2908c56a4daedaf8e67cb1afafafb5d07f985
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-04-29 11:04:49 +0200

    Split SpawnGravityFieldCommand into .h/.cpp

---
+++ Game/GravityBender/SpawnGravityFieldCommand.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <chrono>
+
+#include "Game/GravityBender/GravityRegistry.h"
+#include "Game/GravityBender/SpawnGravityFieldCommand.h"
+
+#include "Minigin/Core/GameObject.h"
+
+namespace bvi::gravity_bender
+{
+	SpawnGravityFieldCommand::SpawnGravityFieldCommand( const dae::core::GameObject* contextPlayer )
+		: m_playerContext{ contextPlayer }
+	{}
+
+	void SpawnGravityFieldCommand::Execute( const float )
+	{
+		if ( !m_playerContext )
+		{
+			assert( m_playerContext && "SpawnGravityFieldCommand requires a valid player context." );
+			return;
+		}
+
+		auto currentTime = std::chrono::steady_clock::now();
+		std::chrono::duration<float> timeSinceLastSpawn = currentTime - m_lastSpawnTime;
+
+		constexpr float spawnCooldown = 1.0f;
+		if ( timeSinceLastSpawn.count() >= spawnCooldown )
+		{
+			const auto origin = m_playerContext->GetTransform().GetWorldPosition();
+
+			constexpr float radius = 100.0f;
+			constexpr float strength = 1'500'000.0f;
+			GravityRegistry::AddNode( { origin, strength, radius * radius } );
+
+			m_lastSpawnTime = currentTime;
+		}
+	}
+
+	const void* SpawnGravityFieldCommand::GetTargetContext() const
+	{
+		return m_playerContext;
+	}
+}
--- Game/GravityBender/SpawnGravityFieldCommand.h
+++ Game/GravityBender/SpawnGravityFieldCommand.h
@@ -1,10 +1,7 @@
 #ifndef BVI_SPAWNGRAVITYFIELDCOMMAND_H
 #define BVI_SPAWNGRAVITYFIELDCOMMAND_H
 
-#include <cassert>
 #include <chrono>
-
-#include "Game/GravityBender/GravityRegistry.h"
 
 #include "Minigin/Core/GameObject.h"
 #include "Minigin/Input/ICommand.h"
@@ -14,38 +11,11 @@ namespace bvi::gravity_bender
 	class SpawnGravityFieldCommand final : public dae::input::ICommand
 	{
 	public:
-		explicit SpawnGravityFieldCommand( const dae::core::GameObject* contextPlayer )
-			: m_playerContext{ contextPlayer }
-		{}
+		explicit SpawnGravityFieldCommand( const dae::core::GameObject* contextPlayer );
 
-		void Execute( const float /*deltaTime*/ ) override
-		{
-			if ( !m_playerContext )
-			{
-				assert( m_playerContext && "SpawnGravityFieldCommand requires a valid player context." );
-				return;
-			}
+		void Execute( const float /*deltaTime*/ ) override;
 
-			auto currentTime = std::chrono::steady_clock::now();
-			std::chrono::duration<float> timeSinceLastSpawn = currentTime - m_lastSpawnTime;
-
-			constexpr float spawnCooldown = 1.0f;
-			if ( timeSinceLastSpawn.count() >= spawnCooldown )
-			{
-				const auto origin = m_playerContext->GetTransform().GetWorldPosition();
-
-				constexpr float radius = 100.0f;
-				constexpr float strength = 1'500'000.0f;
-				GravityRegistry::AddNode( { origin, strength, radius * radius } );
-
-				m_lastSpawnTime = currentTime;
-			}
-		}
-
-		[[nodiscard]] const void* GetTargetContext() const override
-		{
-			return m_playerContext;
-		}
+		[[nodiscard]] const void* GetTargetContext() const override;
 
 	private:
 		const dae::core::GameObject* m_playerContext;
