388e6e760f704a22ef2f3f198f8716ec793324c4
Jaro De Graeve <jaro.de.graeve@outlook.com>
2026-05-13 15:39:23 +0200

    ServiceLocator: forward-declare ISoundSystem

--- Minigin/Core/ServiceLocator.h
+++ Minigin/Core/ServiceLocator.h
@@ -3,11 +3,14 @@
 
 #include <memory>
 
-#include <Minigin/Audio/ISoundSystem.h>
-
 // https://gameprogrammingpatterns.com/service-locator.html
 // C.4: Make a function a member only if it needs direct access to the representation of a class
 // SF.20: Use namespaces to express logical structure
+
+namespace dae::audio
+{
+	class ISoundSystem;
+}
 
 namespace dae::core::service_locator
 {
